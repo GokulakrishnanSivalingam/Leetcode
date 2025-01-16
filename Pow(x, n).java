518e0b1aefb9988f25dfc6a421f7f542d6ddda4f
Gokulakrishnan  S <sivalingamgokulakrishnan@gmail.com>
2025-01-16 13:41:59 +0530

    Create Pow(x, n).java

---
+++ Pow(x, n).java
@@ -0,0 +1,13 @@
+class Solution {
+    public double myPow(double x, int n) {
+        if(n>0)
+        {
+            return Math.pow(x,n);
+        }
+        else
+        {
+            return Math.pow(x,n);
+        }
+        
+    }
+}
